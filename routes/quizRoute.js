81cb1b588f266ec6a2cb40cea8c089696d482c02
HossamSalah1 <125518824+HossamSalah1@users.noreply.github.com>
2024-09-14 16:16:38 +0300

    add crud operation for quiz

--- routes/quizRoute.js
+++ routes/quizRoute.js
@@ -1,8 +1,14 @@
-const {newQuiz} =module.require('../controllers/handelQuiz.js')
+const { newQuiz, deleteQuizById,deleteQuiz, updateQuiz, retriveQuizs, retriveQuizById } = module.require('../controllers/handelQuiz.js')
 const express = module.require('express')
 const router = express.Router();
 const app = express();
 app.use(express.json())
-router.post('/quiz', newQuiz)
+
+router.post('/quiz', newQuiz);
+router.patch('/quiz', updateQuiz);
+router.delete('/quiz',deleteQuiz);
+router.delete('/quiz/:id', deleteQuizById);
+router.get('/quiz', retriveQuizs);
+router.get('/quiz/:id', retriveQuizById);
 
 module.exports = router;
