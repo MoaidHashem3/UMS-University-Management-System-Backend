f9f82ca2ba494ee252a149bcaa560829040e261f
ezzbakry <ezzeldenamer@gmail.com>
2024-10-05 20:59:37 +0300

    Correcting

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,12 +7,12 @@ const router=express.Router();
 const app = express();
 const {auth,restrict}=module.require('../Middlewares/auth')
 app.use(express.json())
-router.get('/',auth,restrict("Admin"),getall)
+router.get('/',auth,restrict("admin"),getall)
 router.post('/register', upload.single('image'), createone);
 router.get('/:id',getByid)
 router.patch('/:id',updateOne)
 router.delete('/:id',deleteOne)
-router.delete('/',auth,restrict("Admin"),deleteall)
+router.delete('/',auth,restrict("admin"),deleteall)
 router.post('/login',login)
 router.post('/uploadUserImage/:id',upload.single('image'),uploadImage);
 module.exports=router;
